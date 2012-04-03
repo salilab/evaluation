5d10fd6e5b139739e3d70d7c7348f3d7d4748178
Ben Webb <ben@salilab.org>
2012-04-03 20:03:01 +0000

    Check our Config subclass.

---
+++ test/backend/test_config.py
@@ -0,0 +1,46 @@
+import unittest
+import evaluation
+import os
+
+class ConfigTest(unittest.TestCase):
+    """Check Config class"""
+    def test_init(self):
+        """Check Config init"""
+        open('config', 'w').write("""
+[general]
+admin_email: test@salilab.org
+service_name: test_service
+socket: test.socket
+
+[backend]
+user: test
+state_file: state_file
+check_minutes: 10
+
+[database]
+db: testdb
+frontend_config: frontend.conf
+backend_config: backend.conf
+[directories]
+install: /
+incoming: /in
+preprocessing: /preproc
+
+[oldjobs]
+archive: 1d
+expire: 1d
+
+[scoring]
+evaluation_script: score_tsvmod.pl
+modeller: modpy.sh
+modeller_script: score_modeller.py
+""")
+        c = evaluation.Config('config')
+        self.assertEqual(c.evaluation_script, 'score_tsvmod.pl')
+        self.assertEqual(c.modeller, 'modpy.sh')
+        self.assertEqual(c.modeller_script, 'score_modeller.py')
+        os.unlink('config')
+
+
+if __name__ == '__main__':
+    unittest.main()
