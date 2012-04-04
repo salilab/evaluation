069bf1a36ac63a1c5cc88d981c29ae050497310e
Ben Webb <ben@salilab.org>
2012-04-04 22:43:05 +0000

    Add basic test of main function.

--- test/backend/test_scoremod.py
+++ test/backend/test_scoremod.py
@@ -9,12 +9,44 @@ class DummyPyLab(object):
 class DummyAutoModel(object):
     pass
 
+class DummyChain(object):
+    def __init__(self, name):
+        self.name = name
+        self.residues = [0,1,2]
+
+class DummyModel(object):
+    def __init__(self):
+        self.chains = [DummyChain(x) for x in ['', 'B']]
+    def assess_normalized_dope(self):
+        return -3.0
+    def assess_ga341(self):
+        return (1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0)
+
 class DummyScripts(object):
-    def complete_pdb(self, *args, **keys):
-        pass
+    error = False
+    def complete_pdb(*args, **keys):
+        if DummyScripts.error:
+            raise ValueError("Bad PDB")
+        return DummyModel()
+    complete_pdb = staticmethod(complete_pdb)
 
 class DummyModeller(object):
-    pass
+    class log(object):
+        def minimal(*args): pass
+        minimal = staticmethod(minimal)
+    class environ(object):
+        class libs:
+            class topology:
+                def read(*args, **keys): pass
+                read = staticmethod(read)
+            class parameters:
+                def read(*args, **keys): pass
+                read = staticmethod(read)
+    class selection(object):
+        def __init__(self, *args):
+            pass
+        def assess_dope(self, *args, **keys):
+            pass
 
 class ScoreModellerTests(saliweb.test.TestCase):
     """Check score_modeller script"""
@@ -57,6 +89,18 @@ class ScoreModellerTests(saliweb.test.TestCase):
 
             sys.argv = ['foo', '--seq_ident', '50.0']
             self.assertRaises(SystemExit, self.scoremod.get_options)
+        finally:
+            sys.argv = old_argv
+
+    def test_main(self):
+        """Test score_modeller main function"""
+        d = saliweb.test.RunInTempDir()
+        old_argv = sys.argv
+        try:
+            sys.argv = ['foo', '--model', 'test.pdb', '--seq_ident', '50.0']
+            open('input.profile_A', 'w').write('1.0\n2.0\n')
+            open('input.profile_B', 'w').write('3.0\n4.0\n')
+            self.scoremod.main()
         finally:
             sys.argv = old_argv
 
