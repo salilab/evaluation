545054d5dc7b49976ff5eb39605480f8b7e8bd40
Ben Webb <ben@salilab.org>
2019-08-06 21:36:54 -0700

    Simplify parsing of Modeller output file

--- frontend/evaluation/results_page.py
+++ frontend/evaluation/results_page.py
@@ -8,10 +8,6 @@ TSVModResult = collections.namedtuple('TSVModResult',
      'rmsd', 'no35', 'ga341', 'ga341_pair', 'ga341_surf', 'ga341_comb',
      'zdope'))
 
-ModellerResult = collections.namedtuple('ModellerResult',
-    ('chain', 'zdope', 'ga341', 'zpair', 'zsurf', 'zcombi', 'seqid',
-     'compactness'))
-
 
 class ResultError(object):
     def __init__(self, error):
@@ -22,10 +18,11 @@ def show_results_page(job):
     errors = []
     tsvmod_results = parse_tsvmod(job, errors)
     modeller_results = parse_modeller(job, errors)
+
     return saliweb.frontend.render_results_template("results_ok.html",
         extra_xml_outputs=['evaluation.xml'],
         tsvmod_results=list(tsvmod_results),
-        modeller_results=list(modeller_results), job=job, errors=errors)
+        modeller_results=modeller_results, job=job, errors=errors)
 
 
 def parse_tsvmod(job, errors):
@@ -42,34 +39,25 @@ def parse_tsvmod(job, errors):
 
 
 def parse_modeller(job, errors):
-    current_chain = None
-    current_fields = dict.fromkeys(ModellerResult._fields)
-    field_map = {'ZDOPE': 'zdope', 'GA341': 'ga341', 'Z-PAIR': 'zpair',
-                 'Z-SURF': 'zsurf', 'Z-COMBI': 'zcombi', 'SeqIdent': 'seqid',
-                 'Compactness': 'compactness'}
+    results = []
+    chains = {}
+
     blank_line = re.compile('\s*$')
-    def update_chain(chain):
-        if chain != current_chain and current_chain is not None:
-            r = ModellerResult._make(
-                current_fields[f] for f in ModellerResult._fields)
-            for key in current_fields.keys():
-                current_fields[key] = None
-            return r
-  
     with open(job.get_path('modeller.results')) as fh:
         for line in fh:
-            if re.match(blank_line, line):  # skip blank lines
+            if blank_line.match(line):  # skip blank lines
                 continue
-            if line.startswith('Error'):
+            elif line.startswith('Error'):
                 e = ResultError(line.rstrip('\r\n'))
                 errors.append(e)
-                yield e
+                results.append(e)
             else:
                 chain, key, value = line.rstrip('\r\n').split()
-                c = update_chain(chain)
-                if c: yield c
-                current_chain = chain
-                current_fields[field_map[key]] = float(value)
-                current_fields['chain'] = chain
-    c = update_chain(None)
-    if c: yield c
+                key = key.replace('-', '').lower()
+                if chain in chains:
+                    chains[chain][key] = float(value)
+                else:
+                    r = {key: float(value), 'chain': chain}
+                    chains[chain] = r
+                    results.append(r)
+    return results
